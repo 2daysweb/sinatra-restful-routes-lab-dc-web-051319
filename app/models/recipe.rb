0110d1bc6c841107f8eb8194231da9960444bf29
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-05 16:09:20 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,11 @@
-#Placeholder for a model
+class Recipe < ActiveRecord::Base 
+
+  attr_accessor :name, :ingredients, :cook_time 
+  
+  def initialize(name)
+    @name = name 
+    @ingredients = ingredients 
+    @cook_time = cook_time 
+  end 
+  
+end 
